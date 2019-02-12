e432a85da8d177af29bcc1c0201ad2d4f7a2b066
Shubham Suresh Patil <40215374+ShubhamSureshPatil@users.noreply.github.com>
2019-02-12 18:18:10 -0500

    Add files via upload

---
+++ arduino/sensors/UltraSonic_Sensor.ino
@@ -0,0 +1,20 @@
+float Inch=0.00;
+float cm=0.00;
+float SonarPin=A0;
+int sensorValue;
+void setup(){
+pinMode(SonarPin,INPUT);
+Serial.begin(9600);
+}
+void loop(){
+sensorValue=analogRead(SonarPin);
+delay(50);
+Inch= (sensorValue*0.497*2);
+cm=Inch*2.54;
+//Serial.println(sensorValue);
+Serial.print(Inch);
+Serial.println("inch");
+//Serial.print(cm);
+//Serial.println(" cm");
+delay(100);
+}
