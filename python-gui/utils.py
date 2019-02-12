7ce6c0d3274adc9c880c03c9249df190b699fd8f
Heethesh Vhavle <heethesh@gmail.com>
2019-02-12 00:33:22 -0500

    Temporary utils

---
+++ python-gui/utils.py
@@ -0,0 +1,34 @@
+import threading
+
+
+def map_value(value, leftMin, leftMax, rightMin=0, rightMax=100):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+
+class StoppableThread(threading.Thread):
+    """Thread class with a stop() method. The thread itself has to check
+    regularly for the stopped() condition."""
+
+    def __init__(self, target=None, args=None, daemon=True):
+        if args:
+            super(StoppableThread, self).__init__(
+                target=target, args=args, daemon=daemon
+            )
+        else:
+            super(StoppableThread, self).__init__(target=target, daemon=daemon)
+
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def stopped(self):
+        return self._stop_event.is_set()
