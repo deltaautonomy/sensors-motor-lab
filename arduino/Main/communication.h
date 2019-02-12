f1b5ac576b7490a53f989431ff9a641a42058153
Heethesh Vhavle <heethesh@gmail.com>
2019-02-12 01:17:41 -0500

    Integrated all except temp and ultrasonic sensor

--- arduino/Main/communication.h
+++ arduino/Main/communication.h
@@ -7,8 +7,6 @@
 // Structure to handle the various outputs to the GUI
 typedef struct TXDataPacket {
     // Boolean data
-    uint8_t button1;
-    uint8_t button2;
     uint8_t slot_encoder;
 
     // Encoder data
@@ -18,29 +16,11 @@ typedef struct TXDataPacket {
     // Temperature sensor
     uint8_t temperature;
 
-    // Sharp sensor
-    uint16_t sharp_distance;
-
     // Ultrasonic sensor
     uint16_t ultrasonic_distance;
 
     // Flex sensor
     uint16_t flex_sensor;
-
-    // Filtered angles
-    float roll_angle;
-    float pitch_angle;
-    float yaw_angle;
-
-    // Raw accelerometer data
-    uint16_t accel_x;
-    uint16_t accel_y;
-    uint16_t accel_z;
-
-    // Raw gyroscope data
-    uint16_t gyro_x;
-    uint16_t gyro_y;
-    uint16_t gyro_z;
 
     // Ouput servo angle
     uint8_t servo_angle;
@@ -58,12 +38,8 @@ typedef struct RXDataPacket {
     uint8_t servo_angle;
 
     // Input motor angle
-    uint16_t motor_angle;
-
-    // DC motor PID gains
-    float motor_kp;
-    float motor_ki;
-    float motor_kd;
+    int16_t motor_angle;
+    int16_t motor_velocity;
 
     // Stepper motor
     uint16_t stepper_value;
